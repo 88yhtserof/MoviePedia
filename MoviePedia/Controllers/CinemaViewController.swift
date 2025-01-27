f02d5106127ffdb661fbc2d2b788c4eab1cf02fc
88yhtserof <88yhtserofg@gmail.com>
2025-01-28 00:47:38 +0900

    [FEAT] Cinema: 프로필 뷰 구성 및 정보 선택 시 프로필 설정 화면으로 전환 #2

--- MoviePedia/Controllers/CinemaViewController.swift
+++ MoviePedia/Controllers/CinemaViewController.swift
@@ -7,10 +7,36 @@
 
 import UIKit
 
-class CinemaViewController: BaseViewController {
+final class CinemaViewController: BaseViewController {
+    
+    private let profileInfoView = ProfileInfoView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureViews()
+        configureHierarchy()
+        configureConstraints()
+    }
+    
+    @objc func presentProfileEditVC() {
+        let profileNicknameEditVC = ProfileNicknameEditViewController()
+        present(profileNicknameEditVC, animated: true)
+    }
+}
+
+private extension CinemaViewController {
+    func configureViews() {
+        profileInfoView.userInfoButton.addTarget(self, action: #selector(presentProfileEditVC), for: .touchUpInside)
+    }
+    
+    func configureHierarchy() {
+        view.addSubviews(profileInfoView)
+    }
+    
+    func configureConstraints() {
+        profileInfoView.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(8)
+        }
     }
 }
