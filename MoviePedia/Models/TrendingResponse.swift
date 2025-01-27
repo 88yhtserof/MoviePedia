7584cc796c406154610a5c0d5405760d75a15dad
88yhtserof <88yhtserofg@gmail.com>
2025-01-27 16:59:00 +0900

    [FEAT] Movie entity 분리 및 반영 #2

--- MoviePedia/Models/TrendingResponse.swift
+++ MoviePedia/Models/TrendingResponse.swift
@@ -9,16 +9,7 @@ import Foundation
 
 struct TrendingResponse: Decodable {
     var page: Int
-    var results: [Trending]
+    var results: [Movie]
 }
 
-struct Trending: Decodable {
-    let id: Int
-    let backdrop_path: String
-    let title: String
-    let overview: String
-    let poster_path: String
-    let genre_ids: [Int]
-    let release_date: String
-    let vote_average: Double
-}
+
