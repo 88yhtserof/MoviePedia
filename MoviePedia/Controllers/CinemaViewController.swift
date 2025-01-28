58ea136fb6a268ff661a25cddc8227c373e1f227
88yhtserof <88yhtserofg@gmail.com>
2025-01-29 00:47:38 +0900

    [FEAT] Cinema: 오늘의 영화 UI 구성(화면 크기에 따라 포스터 이미지 크기 조정) #2

--- MoviePedia/Controllers/CinemaViewController.swift
+++ MoviePedia/Controllers/CinemaViewController.swift
@@ -37,7 +37,9 @@ final class CinemaViewController: BaseViewController {
 private extension CinemaViewController {
     func configureViews() {
         profileInfoView.userInfoButton.addTarget(self, action: #selector(presentProfileEditVC), for: .touchUpInside)
+        
         collectionView.backgroundColor = .clear
+        collectionView.isScrollEnabled = false
         
         recentSearchesRemoveAllButton.setTitle("전체 삭제", for: .normal)
         recentSearchesRemoveAllButton.setTitleColor(.moviepedia_point, for: .normal)
@@ -49,18 +51,21 @@ private extension CinemaViewController {
     
     func configureConstraints() {
         profileInfoView.snp.makeConstraints { make in
-            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(8)
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(8)
         }
         
         collectionView.snp.makeConstraints { make in
             make.top.equalTo(profileInfoView.snp.bottom).offset(8)
-            make.bottom.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(8)
         }
     }
     
     func configureCollectionViewDataSource() {
         let emptyRecentSearchCellRegistration = UICollectionView.CellRegistration(handler: emptyResentSearchCellRegisterHandler)
         let recentSearchCellRegistration = UICollectionView.CellRegistration(handler: recentSearchCellRegidtrationHandler)
+        let todayMovieCellRegidtration = UICollectionView.CellRegistration(handler: todayMovieCellRegidtrationHandler)
         
         dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
             guard let section = Section(rawValue: indexPath.section) else {
@@ -73,6 +78,8 @@ private extension CinemaViewController {
                 cell = collectionView.dequeueConfiguredReusableCell(using: emptyRecentSearchCellRegistration, for: indexPath, item: "최근 검색어 내역이 없습니다.")
             case .recentSeach:
                 cell = collectionView.dequeueConfiguredReusableCell(using: recentSearchCellRegistration, for: indexPath, item: itemIdentifier)
+            case .todayMovie:
+                cell  = collectionView.dequeueConfiguredReusableCell(using: todayMovieCellRegidtration, for: indexPath, item: itemIdentifier)
             }
             
             return cell
@@ -106,11 +113,13 @@ private extension CinemaViewController {
             return sectionForEmptyRecentSearch()
         case .recentSeach:
             return sectionForRecentSearch()
+        case .todayMovie:
+            return sectionForTodayMovie()
         }
     }
     
     func sectionForEmptyRecentSearch() -> NSCollectionLayoutSection {
-        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(50))
+        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(30))
         
         let item = NSCollectionLayoutItem(layoutSize: size)
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitems: [item])
@@ -120,7 +129,7 @@ private extension CinemaViewController {
     }
     
     func sectionForRecentSearch() -> NSCollectionLayoutSection {
-        let size = NSCollectionLayoutSize(widthDimension: .estimated(100.0), heightDimension: .absolute(35))
+        let size = NSCollectionLayoutSize(widthDimension: .estimated(100.0), heightDimension: .absolute(30))
         
         let item = NSCollectionLayoutItem(layoutSize: size)
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitems: [item])
@@ -131,11 +140,17 @@ private extension CinemaViewController {
     }
     
     func sectionForTodayMovie() -> NSCollectionLayoutSection  {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.75), heightDimension: .estimated(1.0))
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(1.0))
+        let (offset, sectionInset, recentSearch, header) = (8.0, 10.0, 30.0, 50.0)
+        let height = collectionView.frame.height - (offset * 2 + sectionInset + recentSearch + header * 2)
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.6), heightDimension: .absolute(height))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
         let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 16
+        section.orthogonalScrollingBehavior = .continuous
+        section.boundarySupplementaryItems = [titleBoundarySupplementaryItem()]
         return section
     }
     
@@ -151,7 +166,7 @@ extension CinemaViewController {
     enum Section: Int, CaseIterable {
         case emptyRecentSearch
         case recentSeach
-//        case todayMovie
+        case todayMovie
     }
     
     typealias DataSource = UICollectionViewDiffableDataSource<Section, String>
@@ -165,19 +180,30 @@ extension CinemaViewController {
         cell.configure(with: item)
     }
     
+    func todayMovieCellRegidtrationHandler(cell: TodayMovieCollectionViewCell, indexPath: IndexPath, item: String) {
+        let movie = Movie(id: 1, backdrop_path: "", title: "나 홀로 집에", overview: "크리스마스 시즌의 시카고. 말썽꾸러기라 집안 가족들로부터 욕을 듣고 따돌림 당하는 케빈은 늘 자신은 혼자 살거라면서 가족들이 모두 없어졌으면 좋겠다고 생각한다. 자기의 치즈 피자를 먹은 형과 싸워 소동을 일으키자 엄마는 케빈을 3층 다락방으로 올려보낸다. 케빈의 가족들과 케빈의 집에 온 손님들은 다음 날에 크리스마스 연휴를 이용해 프랑스의 친척 집으로 떠날 계획이었다. 그날 밤, 바람이 세차게 불어 전화선과 전기선이 끊긴다. 케빈의 가족들은 늦잠을 자게 되어 비행기 시간을 맞추기 위해 허둥대다가 그만 3층 다락방에서 잠이 든 케빈을 두고 떠난다. 잠에서 깬 케빈은 혼자 남은 것을 알고 하느님이 자신의 소원을 들어주었다고 기뻐한다. 비행기를 타고 가던 케빈의 어머니는 무엇인가 빠뜨린 기분에 고민하다가 케빈을 두고 왔음에 놀란다. 하지만 전화선이 불통이라, 어쩔 수 없다가 프랑스에 도착한 식구들은 목적지로 가고 엄마는 케빈이 걱정이 되어 집으로 돌아갈 비행기표를 사기 위해 안간힘을 쓰지만 연말연휴라 좌석이 없었다. 혼자 집에 남은 케빈은 형과 누나 방을 구경하면서 즐거워한다. 그리고 노래를 부르고 트리도 만들면서 자축한다. 그런데 빈집털이 2인조 도둑이 케빈의 집을 호시탐탐 노리고 있다는 것을 알게 되는데...", poster_path: "", genre_ids: [], release_date: "", vote_average: 0)
+        cell.configure(with: movie)
+    }
+    
     func headerSupplementaryRegistrationHandler(supplementaryView: TitleSupplementaryView, string: String, indexPath: IndexPath) {
-        supplementaryView.addRightAccessoryView(recentSearchesRemoveAllButton)
-        supplementaryView.configure(with: "최근 검색어")
+        if indexPath.section == 2 {
+            supplementaryView.configure(with: "오늘의 영화")
+            
+        } else {
+            supplementaryView.addRightAccessoryView(recentSearchesRemoveAllButton)
+            supplementaryView.configure(with: "최근 검색어")
+        }
     }
     
     func createSnapshot() {
         snapshot = Snapshot()
-        snapshot.appendSections([.emptyRecentSearch, .recentSeach])
+        snapshot.appendSections(Section.allCases)
         if recentSearches.isEmpty {
             snapshot.appendItems(["0"], toSection: .emptyRecentSearch)
         } else {
             snapshot.appendItems(recentSearches, toSection: .recentSeach)
         }
+        snapshot.appendItems(["1", "2", "3", "4", "5"], toSection: .todayMovie)
         dataSource.apply(snapshot)
     }
 }
