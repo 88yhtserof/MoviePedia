12ae96b5467984a32f5369c4753df912588d5e93
88yhtserof <88yhtserofg@gmail.com>
2025-01-31 19:44:18 +0900

    [FEAT] MovieDetail: 시놉시스 섹션 UI 구현 / more 버튼 동작 구현 #4

--- MoviePedia/Controllers/MovieDetailViewController.swift
+++ MoviePedia/Controllers/MovieDetailViewController.swift
@@ -50,16 +50,35 @@ private extension MovieDetailViewController {
     
     func configureCollectionViewDataSource() {
         let backdropCellRegistration = UICollectionView.CellRegistration(handler: backdropCellRegistrationHandler)
+        let synopsisCellRegistration = UICollectionView.CellRegistration(handler: synopsisCellRegistrationHandler)
         
         dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
-            let cell = collectionView.dequeueConfiguredReusableCell(using: backdropCellRegistration, for: indexPath, item: itemIdentifier.backdrop)
+            guard let section = Section(rawValue: indexPath.section) else {
+                fatalError("Unknown section")
+            }
+            var cell: UICollectionViewCell
+            switch section {
+            case .backdrop:
+                cell = collectionView.dequeueConfiguredReusableCell(using: backdropCellRegistration, for: indexPath, item: itemIdentifier.backdrop)
+            case .synopsys:
+                cell = collectionView.dequeueConfiguredReusableCell(using: synopsisCellRegistration, for: indexPath, item: itemIdentifier.synopsis)
+            }
+            
             return cell
         })
         
         let backdropFooterSupplementaryProvider = UICollectionView.SupplementaryRegistration(elementKind: "layout-footer-element-kind", handler: movieDeatilInfoSupplemetaryRegistrationHandler)
+        let headerSupplementaryProvider = UICollectionView.SupplementaryRegistration(elementKind: "title-element-kind", handler: headerSupplementaryRegistrationHandler)
         
         dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
-            return collectionView.dequeueConfiguredReusableSupplementary(using: backdropFooterSupplementaryProvider, for: indexPath)
+            switch kind {
+            case "title-element-kind":
+                return collectionView.dequeueConfiguredReusableSupplementary(using: headerSupplementaryProvider, for: indexPath)
+            case "layout-footer-element-kind":
+                return collectionView.dequeueConfiguredReusableSupplementary(using: backdropFooterSupplementaryProvider, for: indexPath)
+            default:
+                return UICollectionReusableView()
+            }
         }
         
         createSnapshot()
@@ -71,13 +90,36 @@ private extension MovieDetailViewController {
 private extension MovieDetailViewController {
     enum Section: Int {
         case backdrop
-//        case synopsys
+        case synopsys
 //        case cast
 //        case poster
+        
+        var header: String? {
+            switch self {
+            case .backdrop:
+                return nil
+            case .synopsys:
+                return "Synopsis"
+            }
+        }
     }
     
     struct Item: Hashable {
         let backdrop: String?
+        let synopsis: String?
+        
+        private init(backdrop: String?, synopsis: String?) {
+            self.backdrop = backdrop
+            self.synopsis = synopsis
+        }
+        
+        init(backdrop: String) {
+            self.init(backdrop: backdrop, synopsis: nil)
+        }
+        
+        init(synopsis: String) {
+            self.init(backdrop: nil, synopsis: synopsis)
+        }
     }
     
     typealias DataSource = UICollectionViewDiffableDataSource<Section, Item>
@@ -90,21 +132,59 @@ private extension MovieDetailViewController {
         }
     }
     
+    func synopsisCellRegistrationHandler(cell: TextCollectionViewCell, indexPath: IndexPath, item: String) {
+        cell.text = item
+        cell.numberOfLines = 3
+    }
+    
     func movieDeatilInfoSupplemetaryRegistrationHandler(supplementaryView: MovieDetailInfoSupplementaryView, string: String, indexPath: IndexPath) {
         if indexPath.section == 0 {
             let dateStr = "2024-12-24"
             let ratingStr = "8.0"
             let genreStr = String(format: "%@, %@", "액션", "스릴러")
             supplementaryView.configure(with: [dateStr, ratingStr, genreStr])
-//            supplementaryView.pageControlHandle
         }
     }
     
+    func headerSupplementaryRegistrationHandler(supplementaryView: TitleSupplementaryView, string: String, indexPath: IndexPath) {
+        guard let section = Section(rawValue: indexPath.section) else {
+            fatalError("Unknown section")
+        }
+        
+        switch section {
+        case .backdrop:
+            return
+        case .synopsys:
+            let moreButton = MoreButton()
+            moreButton.tag = indexPath.item
+            moreButton.addTarget(self, action: #selector(moreButtonTapped), for: .touchUpInside)
+            supplementaryView.addRightAccessoryView(moreButton)
+            supplementaryView.configure(with: section.header!)
+        }
+    }
+    
+    @objc func moreButtonTapped(_ sender: UIButton) {
+        let indexPath = IndexPath(item: sender.tag, section: 1)
+        guard let cell = collectionView.cellForItem(at: indexPath) as? TextCollectionViewCell else {
+            print("Could not find cell")
+            return
+        }
+        
+        if sender.isSelected {
+            cell.numberOfLines = 0
+        } else {
+            cell.numberOfLines = 3
+        }
+        dataSource.apply(snapshot)
+    }
+    
     func createSnapshot() {
-        let items = imageList.map{ Item(backdrop: $0) }
+        let backdropItems = imageList.map{ Item(backdrop: $0) }
+        let synopsysItems = [Item(synopsis: "’자윤’이 사라진 뒤, 정체불명의 집단의 무차별 습격으로 마녀 프로젝트가 진행되고 있는 ‘아크’가 초토화된다. 그곳에서 홀로 살아남은 ‘소녀’는 생애 처음 세상 밖으로 발을 내딛고 우연히 만난 ‘경희’의 도움으로 농장에서 지내며 따뜻한 일상에 적응해간다. 한편, ‘소녀’가 망실되자 행방을 쫓는 책임자 ‘장’과 마녀 프로젝트의 창시자 ‘백총괄’의 지령을 받고 제거에 나선 본사 요원 ‘조현’, ‘경희’의 농장 소유권을 노리는 조직의 보스 ‘용두’와 상해에서 온 의문의 4인방까지 각기 다른 목적을 지닌 세력이 하나 둘 모여들기 시작하면서 ‘소녀’ 안에 숨겨진 본성이 깨어나는데… 모든 것의 시작, 더욱 거대하고 강력해진 마녀가 온다.")]
         snapshot = Snapshot()
-        snapshot.appendSections([.backdrop])
-        snapshot.appendItems(items, toSection: .backdrop)
+        snapshot.appendSections([.backdrop, .synopsys])
+        snapshot.appendItems(backdropItems, toSection: .backdrop)
+        snapshot.appendItems(synopsysItems, toSection: .synopsys)
         dataSource.apply(snapshot)
     }
 }
@@ -137,8 +217,8 @@ private extension MovieDetailViewController {
         switch section {
         case .backdrop:
             return sectionForBackdrop()
-//        case .synopsys:
-//            return sectionForSynopsys()
+        case .synopsys:
+            return sectionForSynopsys()
 //        case .cast:
 //            return sectionForCast()
 //        case .poster:
@@ -157,13 +237,14 @@ private extension MovieDetailViewController {
         return section
     }
     
-//    func sectionForSynopsys() -> NSCollectionLayoutSection {
-//        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(100))
-//        let item = NSCollectionLayoutItem(layoutSize: size)
-//        let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitems: [item])
-//        let section = NSCollectionLayoutSection(group: group)
-//        return section
-//    }
+    func sectionForSynopsys() -> NSCollectionLayoutSection {
+        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(500))
+        let item = NSCollectionLayoutItem(layoutSize: size)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.boundarySupplementaryItems = [titleBoundarySupplementaryItem()]
+        return section
+    }
 //    
 //    func sectionForCast() -> NSCollectionLayoutSection {
 //        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.4), heightDimension: .absolute(50))
@@ -191,5 +272,11 @@ private extension MovieDetailViewController {
         let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                                heightDimension: .absolute(50))
         return NSCollectionLayoutBoundarySupplementaryItem(layoutSize: size, elementKind: "layout-footer-element-kind", alignment: .bottom)
+    }
+    
+    func titleBoundarySupplementaryItem() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let titleSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .absolute(50))
+        return NSCollectionLayoutBoundarySupplementaryItem(layoutSize: titleSize, elementKind: "title-element-kind", alignment: .top)
     }
 }
